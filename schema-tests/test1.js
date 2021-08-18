47b270898c4bc808c5e79e948631b75db7e15e75
mrzou <382066178@qq.com>
2021-08-19 00:34:41 +0800

    update

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -2,19 +2,54 @@
  * @Author: mrzou
  * @Date: 2021-08-18 17:06:28
  * @LastEditors: mrzou
- * @LastEditTime: 2021-08-18 18:04:46
+ * @LastEditTime: 2021-08-19 00:33:38
  * @Description: file content
  */
-const Ajv = require("ajv")
-
-const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+const Ajv = require("ajv").default
 const addFormats = require("ajv-formats")
+const lcalize = require("ajv-i18n")
 
+const ajv = new Ajv({ allErrors: true /* jsonPointers: true */ }) // options can be passed, e.g. {allErrors: true}
+
+require("ajv-errors")(ajv /*, {singleError: true} */)
 addFormats(ajv)
+// ajv.addFormat("test", (data) => {
+//   console.log(data, "-----")
+//   return data === "haha"
+// })
 
-ajv.addFormat("test", (data) => {
-  console.log(data, "-----")
-  return data === "haha"
+ajv.addKeyword("test", {
+  // validate: function fun(schema, data) {
+  //   // console.log(schema, data)
+  //   // if (schema) {
+  //   //   return true
+  //   // } else {
+  //   //   return data.length === 6
+  //   // }
+  //   // fun.errors = [
+  //   //   {
+  //   //     instancePath: "/name",
+  //   //     schemaPath: "#/properties/name/test",
+  //   //     keyword: "test666",
+  //   //     params: {},
+  //   //     // message: '应当通过 "test 关键词校验"',
+  //   //   },
+  //   // ]
+  //   return false
+  // },
+  // keyword: "test",
+  // compile(sch, parentSchema) {
+  //   console.log(sch, parentSchema)
+  //   return () => true
+  // },
+  // metaSchema: {
+  //   type: "boolean", //test 的赋值类型限制
+  // },
+  macro() {
+    return {
+      minLength: 10,
+    }
+  },
 })
 
 // const schema = {
@@ -34,8 +69,15 @@ const schema = {
     name: {
       type: "string",
       //   format: "email",
-      format: "test",
-      //   minLength: 10,
+      // format: "test",
+      // test: true, // addKeyword
+      minLength: 10,
+      additionalProperties: false,
+      errorMessage: {
+        type: "这是不对的",
+        minLength: "长度最小必须10位",
+        additionalProperties: "hahahahah",
+      },
     },
     age: {
       type: "number",
@@ -66,4 +108,7 @@ const valid = validate({
   pets: ["mini", 16],
   isWorker: true,
 })
-if (!valid) console.log(validate.errors)
+if (!valid) {
+  lcalize.zh(validate.errors)
+  console.log(validate.errors)
+}
